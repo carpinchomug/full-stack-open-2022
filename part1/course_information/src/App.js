68160d8eeb505151f2da40e1ef128838cb5de8ba
Akiyoshi Suda <aki.suda@protonmail.com>
2022-04-11 12:03:46 +0900

    initialise part1 with course_information

---
+++ part1/course_information/src/App.js
@@ -0,0 +1,69 @@
+const Header = ({course}) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Part = ({name, exercises}) => {
+  return (
+    <div>
+      <p>{name} {exercises}</p>
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const sum = (array) => {
+    return array.reduce(
+      (previous, current) => previous + current.exercises, 0
+    )
+  }
+
+  return (
+    <div>
+      <p>Number of exercises {sum(parts)}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
