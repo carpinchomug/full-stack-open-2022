acbe9868f3ed7709ab716f96163e439c53b49773
Akiyoshi Suda <aki.suda@protonmail.com>
2022-04-11 22:03:55 +0900

    part1 unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+
+const Heading = ({text}) => (
+  <div>
+    <h1>{text}</h1>
+  </div>
+)
+
+const Button = ({text, handleClick}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+const Statistics = ({good, neutral, bad, allCliks}) => {
+  if (allCliks == 0) {
+    return (
+      <div>
+        <p>No feedback given</p>    
+      </div>
+    )
+  }
+
+  const average = (good + bad) / allCliks
+  const positive = `${good / allCliks * 100} %`
+
+  return (
+    <div>
+      <table>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allCliks, setAll] = useState(0)
+
+  const incrementGood = () => {
+    setGood(good + 1)
+    setAll(allCliks + 1)
+  }
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(allCliks + 1)
+  }
+  const incrementBad = () => {
+    setBad(bad + 1)
+    setAll(allCliks + 1)
+  }
+
+  return (
+    <div>
+      <Heading text="give feedback" />
+      <Button text="good" handleClick={incrementGood} />
+      <Button text="neutral" handleClick={incrementNeutral} />
+      <Button text="bad" handleClick={incrementBad} />
+
+      <Heading text="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} allCliks={allCliks} />
+    </div>
+  )
+}
+
+export default App
